9a064101294b8fa9d5ecde7c4783aab036e6f9d4
AR <aleiphoenix@gmail.com>
2013-08-19 23:00:00 +0800

    init

---
+++ README.markdown
@@ -0,0 +1,11 @@
+## pygrit
+
+pygrit intends to provide a minimal python port from [ruby grit](https://github.com/mojombo/grit).
+
+currently working on diff and commit objects.
+
+## LICENSE
+
+[The MIT License (MIT)](http://opensource.org/licenses/MIT)
+
+Copyright (c) 2013 AR (a.k.a AleiPhoenix)
---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from setuptools import find_packages, setup
+
+version = '%s/VERSION' % os.path.abspath(os.path.dirname(__file__))
+with open(version) as v:
+    VERSION = v.readline().strip()
+
+setup(
+    name = "pygrit",
+    version = VERSION,
+
+    package_dir = {"": "src"},
+    packages = find_packages("src"),
+
+    entry_points = {},
+
+    install_requires = [],
+
+    author = "AR",
+    description = ("pygrit intends to provide a minimal"
+                   "python port from ruby grit."),
+    url = "https://github.com/aleiphoenix/pygrit"
+)
