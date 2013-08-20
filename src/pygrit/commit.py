71abad253c54ac7a7e599fc5abdd86bf5343970b
AR <aleiphoenix@gmail.com>
2013-08-20 16:43:51 +0800

    add datetime, timestamp and offset property to commit object

--- src/pygrit/commit.py
+++ src/pygrit/commit.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from StringIO import StringIO
+from datetime import datetime
 
 from cdiff import PatchStream, DiffParser
 
@@ -7,9 +8,6 @@ from .diff import Diff
 from .utils.wrappers import cached
 
 
-
-class DiffEx(object):
-    pass
 
 class Commit:
 
@@ -23,6 +21,18 @@ class Commit:
         self.head = head
         self.repo = None
 
+    @property
+    def authored_datetime(self):
+        return datetime.fromtimestamp(self.authored_timestamp)
+
+    @property
+    def committed_datetime(self):
+        return datetime.fromtimestamp(self.committed_timestamp)
+
+    @property
+    def datetime(self):
+        return self.committed_timestamp
+
     def _init_from_raw(self, raw_commit):
         """
         init from result of Git.get_commit(), a dict
@@ -30,12 +40,14 @@ class Commit:
         self._raw_commit = raw_commit
         self.id = raw_commit['id']
         self.message = raw_commit['message']
-        self.authored_date = raw_commit['authored_timestamp']
-        self.committed_date = raw_commit['committed_timestamp']
         self.author_name = raw_commit['author_name']
         self.author_email = raw_commit['author_email']
+        self.authored_timestamp = int(raw_commit['authored_timestamp'])
+        self.authored_offset = int(raw_commit['authored_offset'])
         self.committer_name = raw_commit['committer_name']
         self.committer_email = raw_commit['committer_email']
+        self.committed_timestamp = int(raw_commit['committed_timestamp'])
+        self.committed_offset = int(raw_commit['committed_offset'])
         self.parents_id = map(lambda x: x, raw_commit['parents'])
 
     def __repr__(self):
