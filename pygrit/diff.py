402de5df601a0a8eff150c06ce8170d24a4fd5ef
think <microhuang@live.com>
2015-04-25 13:37:25 +0800

    Update diff.py

--- pygrit/diff.py
+++ pygrit/diff.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import re
-from StringIO import StringIO
+from io import StringIO
 
 from cdiff import PatchStream, DiffParser
 from pygrit.ext import encode
